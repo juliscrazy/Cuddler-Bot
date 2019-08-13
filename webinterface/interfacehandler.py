7007c1cc7b602ae750d963eeeb558b333c8d033d
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-08-13 22:41:28 +0200

    Added more links and site pages

--- webinterface/interfacehandler.py
+++ webinterface/interfacehandler.py
@@ -10,6 +10,18 @@ app.debug = False
 def index():
     return flask.render_template("dashboard.html")
 
-@app.route("/reports/")
-def reports():
-    return flask.render_template("reports.html")
+@app.route("/servers/")
+def Servers():
+    return flask.render_template("servers.html")
+
+@app.route("/stats/")
+def stats():
+    return flask.render_template("stats.html")
+
+@app.route("/integrations/")
+def integrations():
+    return flask.render_template("integrations.html")
+
+@app.route("/logs/")
+def logs():
+    return flask.render_template("logs.html")
